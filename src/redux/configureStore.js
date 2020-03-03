11657556fcb308b6bffd8ea2617503481707fcfd
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-03-03 16:40:51 -0300

    Persist user data when reload the page

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState = {}) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const persistConfig = { key: 'targetMVD', storage, whitelist: ['user'] };
 
-  return createStore(
-    rootReducer,
+  let store = createStore(
+    persistReducer(persistConfig, rootReducer),
     initialState,
     composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
+  let persistor = persistStore(store);
+
+  return { store, persistor };
 }
