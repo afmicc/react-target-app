6634e2da9d4d146bb6c2b5c7e3dd479089f73225
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-03-26 10:12:26 -0300

    Manage props

--- src/components/HOCs/withMap.js
+++ src/components/HOCs/withMap.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import TargetsMap from 'components/TargetsMap';
 
-const withMap = Component => () => (
+const withMap = Component => (...props) => (
   <div className="page-container">
-    <Component />
+    <Component {...props} />
     <div className="column home-column-right">
       <TargetsMap />
     </div>
