11e1b9c224a139330ad1db4f754e50cd5b219b29
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-03-27 11:45:32 -0300

    Add not found page

--- src/pages/notFound/NotFoundPage.js
+++ src/pages/notFound/NotFoundPage.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const NotFoundPage = () => {
-  return <div>NotFound</div>;
-};
+import smilies from 'assets/smilies.svg';
+import LandingSidebar from 'components/landingSidebar/LandingSidebar';
+
+const NotFoundPage = () => (
+  <div className="page-container">
+    <div className="column column--side-padding column--center-content">
+      <img src={smilies} alt="smilies"></img>
+      <h1 className="column__header column__header--no-transform">Oops!</h1>
+      <h2 className="column__subheader">Page not found</h2>
+      <p className="column__text">
+        Sorry, but the page you are looking for is not found. Please, make sure you have typed the current URL.
+      </p>
+    </div>
+    <LandingSidebar />
+  </div>
+);
 
 export default NotFoundPage;
